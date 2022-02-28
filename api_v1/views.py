247086f42a30ece3bbe1c263c5ceab61989cb7e4
ader-api <aderapi6@gmail.com>
2022-02-28 09:30:14 +0300

    retrieve view

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class manageStudentRecord(RetrieveUpdateDestroyAPIView):
+    '''
+    This view is used to manage records related to a Student
+    '''
+
+    serializer_class = studentSerializer
+    lookup_url_kwarg = 'studentId'
+    queryset = student.objects.all()
