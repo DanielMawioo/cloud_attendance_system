fc2cd4f0b7755dc4a37619dee65ce36f787f024a
ader-api <aderapi6@gmail.com>
2022-02-28 09:30:50 +0300

    manage attendance view

--- api_v1/views.py
+++ api_v1/views.py
@@ -10,3 +10,13 @@ class manageStudentRecord(RetrieveUpdateDestroyAPIView):
     serializer_class = studentSerializer
     lookup_url_kwarg = 'studentId'
     queryset = student.objects.all()
+
+
+class manageAttendanceRecord(RetrieveUpdateDestroyAPIView):
+    '''
+    This view is used to manage records related to a Attendance
+    '''
+
+    serializer_class = attendanceSerializer
+    lookup_url_kwarg = 'id'
+    queryset = attendance.objects.all()
