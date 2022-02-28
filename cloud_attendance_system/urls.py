2d7d3778602420d84e98405786d992542a7ad97c
ader-api <aderapi6@gmail.com>
2022-02-28 10:36:21 +0300

    update main urls

--- cloud_attendance_system/urls.py
+++ cloud_attendance_system/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from django.contrib.auth.views import LogoutView
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r"^",include("api_v1.urls")),
+    path('accounts/', include('django.contrib.auth.urls')), # new
+    url(r'^logout/$', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
 ]
